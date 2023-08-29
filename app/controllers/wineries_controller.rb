b8212b3a814c77dd024aebab5817e943cf9aac93
Dylan Timmons <dylanstimmons@icloud.com>
2023-08-29 13:37:33 -0600

    Add CRUD functions

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -6,4 +6,36 @@ class WineriesController < ApplicationController
     def show
         @winery = Winery.find(params[:id])
     end
+
+    def new
+        @winery = Winery.new
+    end
+
+    def create
+        @winery = Winery.create(winery_params)
+        redirect_to '/wineries'
+    end
+
+    def edit
+        @winery = Winery.find(params[:id])
+    end
+
+    def update
+        @winery = Winery.find(params[:id])
+        @winery.update(winery_params)
+        redirect_to "/wineries/#{@winery.id}"
+    end
+
+    def destroy
+        @winery = Winery.find(params[:id])
+        @winery.wines.destroy_all
+        @winery.destroy
+        redirect_to '/wineries'
+    end
+
+    private
+
+    def winery_params
+        params.permit(:name, :location, :employees, :still_active)
+    end
 end
