11448f5959369d513533530075f3b2284526ebdd
Dylan Timmons <dylanstimmons@icloud.com>
2023-08-29 13:46:05 -0600

    Add user story 5, 7, 16

--- spec/features/winery_wines/index_spec.rb
+++ spec/features/winery_wines/index_spec.rb
@@ -29,4 +29,22 @@ RSpec.describe "winery wines index page" do
         expect(page).to have_link("Wines from #{winery.name}", href: "/wineries/#{winery.id}/wines")
     end
 
+    #User Story 16
+    it "displays a link to sort wine in alphabetical order" do
+        winery = Winery.create(name: "Castello di Amorosa", location: "California", employees: 48, still_active: true)
+        wine_1 = winery.wines.create(name: "Voyager Vineyard", abv: 15, still_available: true)
+        wine_2 = winery.wines.create(name: "Chardonnay", abv: 14, still_available: false) 
+
+        visit "/wineries/#{winery.id}/wines"
+        
+        expect(page).to have_link("Alphabetize")
+
+        click_link("Alphabetize")
+
+        sorted_wines = [wine_2, wine_1]
+        displayed_wines = page.all("h3").map(&:text)
+
+        expect(displayed_wines).to eq(sorted_wines.map(&:name))
+    end
+
 end
