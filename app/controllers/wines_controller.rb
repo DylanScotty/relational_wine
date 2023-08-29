5a02f7251a1b403f2d7f73569a36301754f8b4be
Dylan Timmons <dylanstimmons@icloud.com>
2023-08-29 13:38:49 -0600

    Add CRUD methods

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -6,4 +6,27 @@ class WinesController < ApplicationController
     def show 
         @wine = Wine.find(params[:id])
     end
+
+    def edit
+        @wine = Wine.find(params[:id])
+    end
+
+    def update
+        wine = Wine.find(params[:id])
+        wine.update(wine_params)
+        redirect_to "/wines/#{wine.id}"
+    end
+
+    def destroy
+        wine = Wine.find(params[:id])
+        wine.destroy
+        redirect_to "/wines"
+    end
+
+
+    private
+
+    def wine_params
+        params.permit(:id, :name, :abv, :still_available)
+    end
 end
