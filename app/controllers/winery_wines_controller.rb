4dfe0b9f97f0a6be83732f0dacef87d8ec8cf996
Dylan Timmons <dylanstimmons@icloud.com>
2023-08-29 13:38:20 -0600

    Add CRUD methods

--- app/controllers/winery_wines_controller.rb
+++ app/controllers/winery_wines_controller.rb
@@ -1,6 +1,28 @@
 class WineryWinesController < ApplicationController
     def index
         @winery = Winery.find(params[:winery_id])
-        @winery_wines = @winery.wines
+        if params.keys.include?("sort")
+            @wines = @winery.wines.order(:name)
+        else
+            @wines = @winery.wines
+        end
+    end
+
+    def new
+        @winery = Winery.find(params[:winery_id])
+    end
+
+    def create
+        @winery = Winery.find(params[:winery_id])
+        wine = @winery.wines.create(wine_params)
+        redirect_to "/wineries/#{@winery.id}/wines"
+    end
+
+
+
+    private
+
+    def wine_params
+        params.permit(:id, :name, :abv, :still_available)
     end
 end
